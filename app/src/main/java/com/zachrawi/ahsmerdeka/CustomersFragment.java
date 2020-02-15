befbcd9e2f6dab67b9a88e0207adcf361509beb1
Zachrawi Al Farizi <zachrawi@ngoprekweb.com>
2020-02-15 11:19:09 +0700

    Add Customer UI

--- app/src/main/java/com/zachrawi/ahsmerdeka/CustomersFragment.java
+++ app/src/main/java/com/zachrawi/ahsmerdeka/CustomersFragment.java
@@ -2,6 +2,9 @@ package com.zachrawi.ahsmerdeka;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,5 +17,31 @@ public class CustomersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_customers, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment_customers, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuAdd:
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameLayout, new AddCustomerFragment())
+                        .commit();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/zachrawi/ahsmerdeka/MainActivity.java
+++ app/src/main/java/com/zachrawi/ahsmerdeka/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
     }
 
     @Override
